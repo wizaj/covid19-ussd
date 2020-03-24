4591d76eb8aa96ff634aba43ae36975f7ea7d19f
Wiza Jalakasi <wizaj@Wz-MBP.local>
2020-03-24 17:53:36 +0300

    Building form logic

--- models/registrant.js
+++ models/registrant.js
@@ -2,22 +2,30 @@
 
 module.exports = function(sequelize, DataTypes) {
   var Registrant = sequelize.define('Registrant', {
-    first_name: {
+    has_fever: {
       type: DataTypes.STRING,
       allowNull: false
     },
-    last_name: {
+    is_coughing: {
       type: DataTypes.STRING,
       allowNull: true
     },
-    phone_number: {
+    breathing_issues: {
       type: DataTypes.STRING,
       unique: true
     },
-    language_pref: {
+    travel_last_14_days: {
       type: DataTypes.STRING,
       allowNull: false
     },
+    met_with_corona_patient: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    neighbourhood_or_estate: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   });
   return Registrant;
 };
